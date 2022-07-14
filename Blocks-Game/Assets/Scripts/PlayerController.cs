70cc96ba08c03636ef5e37dde3bccfd9dea8c0ae
Raylock <39481834+RaylockOblivion@users.noreply.github.com>
2022-07-14 10:33:54 +0530

    Obstacles and winBoxes

--- Blocks-Game/Assets/Scripts/PlayerController.cs
+++ Blocks-Game/Assets/Scripts/PlayerController.cs
@@ -7,33 +7,118 @@ public class PlayerController : MonoBehaviour
 
     public Rigidbody2D rBD;
     public float speed;
+    private float acceleration;
+    public float maxSpeed;
+
+    private bool isMovingLeft, isMovingRight, isMovingUp, isMovingDown, isBrakeApplied;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isMovingDown = isMovingLeft = isMovingRight = isMovingUp = false;
+        isBrakeApplied = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Brake Applied");
+            acceleration = 0;
+            rBD.velocity = new Vector2(0, 0);
+            isMovingRight = false;
+            isMovingLeft = false;
+            isMovingUp = false;
+            isMovingDown = false;
+            isBrakeApplied = false;
+            return;
+        }
         if (Input.GetAxis("Horizontal")>0)
         {
-            rBD.velocity = new Vector2(speed, 0);
-        }else if (Input.GetAxis("Horizontal") < 0)
+            if (acceleration < maxSpeed)
+                acceleration = acceleration + speed;
+            else
+                acceleration = maxSpeed;
+            isMovingRight = true;
+            isMovingLeft = false;
+            isMovingUp = false;
+            isMovingDown = false;
+            isBrakeApplied = false;
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
         {
-            rBD.velocity = new Vector2(-speed, 0);
+            if (acceleration < maxSpeed)
+                acceleration = acceleration + speed;
+            else
+                acceleration = maxSpeed;
+            isMovingRight = false;
+            isMovingLeft = true;
+            isMovingUp = false;
+            isMovingDown = false;
+            isBrakeApplied = false;
         }
         else if (Input.GetAxis("Vertical") > 0)
         {
-            rBD.velocity = new Vector2(0,speed);
+            if (acceleration < maxSpeed)
+                acceleration = acceleration + speed;
+            else
+                acceleration = maxSpeed;
+            isMovingRight = false;
+            isMovingLeft = false;
+            isMovingUp = false;
+            isMovingDown = true;
+            isBrakeApplied = false;
         }
         else if (Input.GetAxis("Vertical") < 0)
         {
-            rBD.velocity = new Vector2(0,-speed);
+            if (acceleration < maxSpeed)
+                acceleration = acceleration + speed;
+            else
+                acceleration = maxSpeed;
+            isMovingRight = false;
+            isMovingLeft = false;
+            isMovingUp = true;
+            isMovingDown = false;
+            isBrakeApplied = false;
         }
         else
         {
-            rBD.velocity = new Vector2(0, 0);
+            isBrakeApplied = true;
         }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isBrakeApplied)
+        {
+            if (acceleration > 0)
+                acceleration -= speed;
+            else
+                acceleration = 0;
+        }
+        if (isMovingLeft)
+        {
+            rBD.velocity = new Vector2(-acceleration, 0);
+        }
+        else if (isMovingRight)
+        {
+            rBD.velocity = new Vector2(acceleration, 0);
+        }
+        else if (isMovingUp)
+        {
+            rBD.velocity = new Vector2(0, -acceleration);
+        }
+        else if (isMovingDown)
+        {
+            rBD.velocity = new Vector2(0, acceleration);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag=="WinBox")
+            Debug.Log("Level Complete!!");
     }
 }
